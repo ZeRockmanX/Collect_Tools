314ccd8a248e5561b9087a1cd30d7b0822a2a7bd
ZeRockmanX <ZeRockmanX@gmail.com>
2018-05-21 23:54:02 +0800

    merge two project

--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,17 @@
-module.exports = function ( app ) {
+var MongoClient = require('mongodb').MongoClient;
+module.exports = function (app) {
     app.get('/home', function (req, res) {
-        if(req.session.user){
+        if (req.session.user) {
             var Commodity = global.dbHelper.getModel('commodity');
             Commodity.find({}, function (error, docs) {
-                res.render('home',{spiderMessage: null,Commoditys:null});
+                res.render('home', {spiderMessage: null, Commoditys: null, items: null, flag: null});
             });
-        }else{
+        } else {
             req.session.error = "login again";
             res.redirect('/login');
         }
     });
-    app.get('/addcommodity', function(req, res) {
+    app.get('/addcommodity', function (req, res) {
         res.render('addcommodity');
     });
     app.post('/addcommodity', function (req, res) {
@@ -22,12 +23,11 @@ module.exports = function ( app ) {
         }, function (error, doc) {
             if (doc) {
                 res.sendStatus(200);
-            }else{
+            } else {
                 res.sendStatus(404);
             }
         });
     });
-
 
 
 //npm install cheerio --save(类似jquery的选择器)
@@ -40,18 +40,58 @@ module.exports = function ( app ) {
         let article_id = "";
         let action = request.query.action;
         var Commodity = global.dbHelper.getModel('commodity');
-        var docs ="";
+        var docs = "";
         Commodity.find({}, function (error, find_docs) {
             docs = find_docs;//Commoditys:null
         });
         switch (action) {
             case "clear":
-                response.render('home', {title: 'Spider', spiderMessage: null,Commoditys:null});
+                response.render('home', {
+                    title: 'Spider',
+                    spiderMessage: null,
+                    Commoditys: null,
+                    items: null,
+                    flag: null
+                });
+                break;
+            case "chart":
+                MongoClient.connect('mongodb://localhost:27017').then((client) => {
+                    let dateMap = {};
+                    client.db('spider_rakuten').collection('Article_1727').find().forEach((doc) => {
+                        if (dateMap[doc.reviewDate]) {
+                            dateMap[doc.reviewDate] += 1;
+                        } else {
+                            dateMap[doc.reviewDate] = 1;
+                        }
+                    }, (error) => {
+                        let items = [];
+                        for (let d in dateMap) {
+                            items.push({
+                                x: d,
+                                y: dateMap[d]
+                            });
+                        }
+                        response.render('home', {
+                            items: items,
+                            title: 'Spider',
+                            spiderMessage: null,
+                            Commoditys: null,
+                            flag: true
+                        });
+
+                    });
+                });
                 break;
             case "getComments":
                 article_id = request.query.id;
                 let resData = await apiFunc.getById(article_id);
-                response.render('home', {title: 'Spider', spiderMessage: JSON.stringify(resData),Commoditys:null});
+                response.render('home', {
+                    title: 'Spider',
+                    spiderMessage: JSON.stringify(resData),
+                    Commoditys: null,
+                    items: null,
+                    flag: false
+                });
                 break;
             case "collectComments":
                 article_id = request.query.id;
@@ -59,16 +99,28 @@ module.exports = function ( app ) {
                     let itemUrl = baseUrl + article_id + "/";
                     await apiFunc.collectById(itemUrl, article_id);
                     console.log("ok");
-                    response.render('home', {title: 'Spider', spiderMessage: 'collect Comments success',Commoditys:null});
+                    response.render('home', {
+                        title: 'Spider',
+                        spiderMessage: 'collect Comments success',
+                        Commoditys: null,
+                        items: null,
+                        flag: null
+                    });
                 } catch (error) {
                     console.log("collectComments error:");
                     console.log(error);
-                    response.render('home', {title: 'Spider', spiderMessage: 'collect Comments failed',Commoditys:null});
+                    response.render('home', {
+                        title: 'Spider',
+                        spiderMessage: 'collect Comments failed',
+                        Commoditys: null,
+                        items: null,
+                        flag: null
+                    });
                 }
                 break;
             default:
         }
-        response.end();
+        //response.end();
     });
 
 }
